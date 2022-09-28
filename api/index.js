cc1f28c55bb371d23b87d2903ce40f8016d3a821
이근호B <lgh7010@krafton.com>
2022-09-28 18:49:23 +0900

    CORS에러 수정

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const app = express()
 
 const createError = require('http-errors')
 
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
